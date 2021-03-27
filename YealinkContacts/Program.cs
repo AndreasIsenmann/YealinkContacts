3a11d6914657dbdd44f1041712e1b00a420dfaa4
Andreas Isenmann <andreas.isenmann@ise-world.de>
2021-03-27 11:22:50 +0100

    First basic implementation

--- YealinkContacts/Program.cs
+++ YealinkContacts/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace YealinkContacts
 {
@@ -6,7 +7,75 @@ namespace YealinkContacts
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line;
+            YealinkContact yc = null;
+            StreamReader sr = new StreamReader("vCards.vcf");
+            StreamWriter sw = new StreamWriter("YealinkContacts.xml");
+
+            sw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+            sw.WriteLine("<root_group>");
+            sw.WriteLine("<group display_name=\"All Contacts\" />");
+            sw.WriteLine("<group display_name=\"Blacklist\" />");
+            sw.WriteLine("</root_group>");
+
+
+            sw.WriteLine("<root_contact>");
+            while (null != (line = sr.ReadLine()))
+            {
+                if(null==yc)
+                {
+                    yc = new YealinkContact();
+                }
+
+                if(line.StartsWith("N:"))
+                {
+                    ExtractName(line.Substring(2), yc);
+                }
+
+                if (line.StartsWith("TEL;"))
+                {
+                    ExtractPhoneNumbers(line.Substring(4), yc);
+                }
+
+                if (line.StartsWith("END:VCARD"))
+                {
+                    var xml = yc.GetXmlString();
+                    if(xml!=string.Empty)
+                    {
+                        sw.WriteLine(xml);
+                    }
+
+                    yc = null;
+                }
+
+            }
+            sw.WriteLine("<root_contact>");
+        }
+
+        private static void ExtractName(string line, YealinkContact yc)
+        {
+            var info = line.Split(';');
+            yc.FullName = info[0] + " " + info[1];
+        }
+
+        private static void ExtractPhoneNumbers(string line, YealinkContact yc)
+        {
+            if (line.IndexOf("type=CELL;") >= 0)
+            {
+                yc.Mobile = line.Substring(line.LastIndexOf(":") + 1);
+            }
+            else if (line.IndexOf("type=HOME;") >= 0 || line.IndexOf("type=OTHER;") >= 0)
+            {
+                yc.PhoneHome = line.Substring(line.LastIndexOf(":") + 1);
+            }
+            else if (line.IndexOf("type=WORK;") >= 0 || (line.IndexOf("type=MAIN;") >= 0 && yc.PhoneOffice == string.Empty))
+            {
+                yc.PhoneOffice = line.Substring(line.LastIndexOf(":") + 1);
+            }
+            else
+            {
+
+            }
         }
     }
 }
---
+++ YealinkContacts/YealinkContact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace YealinkContacts
+{
+    public class YealinkContact
+    {
+        public string FullName { get; set; }
+
+        public string PhoneOffice { get; set; }
+        public string PhoneHome { get; set; }
+        public string Mobile { get; set; }
+
+        public YealinkContact()
+        {
+            PhoneHome = string.Empty;
+            PhoneOffice = string.Empty;
+            Mobile = string.Empty;
+        }
+
+        public string GetXmlString()
+        {
+            if (PhoneOffice == string.Empty && PhoneHome == string.Empty && Mobile == string.Empty)
+                return string.Empty;
+
+            return string.Format("<contact display_name=\"{0}\" office_number=\"{1}\" mobile_number=\"{2}\" other_number=\"{3}\" line=\"0\" ring=\"Auto\" default_photo=\"Default: default_contact_image.png\" selected_photo=\"0\" group_id_name=\"All Contacts\" />",
+                FullName, PhoneOffice, Mobile, PhoneHome);
+        }
+    }
+}
